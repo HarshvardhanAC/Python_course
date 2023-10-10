b396672070fe996e258ed4158f3923a665c167f3
HarshVardhan <HarshVardhan123@gmail.com>
2023-10-10 10:27:56 +0530

    modified files added

--- Class_objects.py
+++ Class_objects.py
@@ -1,15 +1,23 @@
 class person :
-    # name = "Harry"
-    # occupation = "Software Developer"
-    # Networth = 10
+    name = "Harry"
+    occupation = "Software developer "
+    income = "100000"
     
-    def info (self):
-        print(f"{self.name} is a {self.occupation} ")
+    def info():
+        print(f"The emolyee name is {self.name} and he is a {self.occupation} with income of {self.income} ")
     
-    
-a = person()
-a.name = 'Harshvardhan'
-a.occupation = "Billioner"
-# a.Networth = "World Richest Person" 
-# print(a.name,a.Networth,a.occupation)
-a.info()
+a= person()
+b=person()
+
+
+a.name = "shubham"
+a.occupation = "Accountant"
+a.income = "15000"
+
+
+b.name = "Harish"
+b.occupation = "Teacher"
+b.income = "20000"
+
+
+b.info()
--- constructor.py
+++ constructor.py
@@ -1,10 +1,24 @@
-class profession:
-    name = "Sakshi"
-    Occupation = "Bussiness Development Executive"
+class proffesion :
     
-    def data (self):
-        print(f"Our Firend {self.name} is {self.Occupation}")
+    def __init__(self,name,occupation) :
+        self.name = name
+        self.occupation = occupation
         
-a= profession()
+    def info(self) :
+            print(f'Your name is {self.name} your work profession is {self.occupation}')
+            
+a= proffesion("Harsh","Learner")
 
-a.data()
+a.info()
+class addition :
+    
+    def __init__(self,a,b) :
+        
+        self.a = a
+        self.b = b
+        
+    def add(self,a,b):
+      print(f"the sum of {self.a} and {self.b} is =",(a+b))
+      
+c= addition(5,6)
+c.add(5,6)
